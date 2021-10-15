05ddab898f4d648ddc276698fa09e0e8e9a1fb50
Maria Sanchez <mariasanchez@Marias-MacBook-Air.local>
2021-10-15 17:28:53 -0500

    web project intialized

---
+++ src/test/java/com/pluralsight/bikeshop/BikeshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.bikeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
